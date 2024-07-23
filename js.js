0caf98273e14b06dddd53dd15d2a80f149ce64ca
Jaydeep <jaydeeprajtech@gmail.com>
2024-07-23 20:29:32 +0530

    login form update

--- js.js
+++ js.js
@@ -1,25 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cards = document.querySelectorAll('.card');
-  
-    cards.forEach(card => {
-      VanillaTilt.init(card, {
-        max: 15,
-        speed: 400,
-        glare: true,
-        'max-glare': 0.2,
-      });
-  
-      const handleMouseMove = (e) => {
-        let x = e.pageX - (window.innerWidth / 2);
-        let y = e.pageY - (window.innerHeight / 2);
-        card.style.transform = `translate(${x / 40}px, ${y / 40}px)`;
-      };
-  
-      window.addEventListener('mousemove', handleMouseMove);
-  
-      return () => {
-        window.removeEventListener('mousemove', handleMouseMove);
-      };
-    });
-  });
-  
+function formregopen() {
+  const element = document.getElementById("formwindow");
+  element.classList.remove("formwindowactive");  // Add newone class
+  element.classList.toggle("formwindow");  // Remove mystyle class
+}
+function formregclose(){
+  console.log("hello");
+  const element = document.getElementById("formwindow");
+  element.classList.toggle("formwindow"); 
+}
